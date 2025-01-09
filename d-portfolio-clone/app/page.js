ac63f38647a1f068c2225f4008bbae4e656e1f32
Abdullah <abdullahansir8644@gmail.com>
2025-01-09 18:32:14 +0500

    Making Navbar

---
+++ d-portfolio-clone/app/page.js
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const home = () => {
+  return (
+    <>
+      <header className=" h-fit px-16 sticky top-4 mx-9 ">
+        <Navbar />
+      </header>
+      <main className=""></main>
+    </>
+  );
+};
+
+export default home;
