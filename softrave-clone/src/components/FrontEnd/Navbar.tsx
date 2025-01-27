f84db53b4d8eac529864660d8b091c23fa9cb5ff
Abdullah <abdullahansir8644@gmail.com>
2025-01-27 23:50:56 +0500

    Added Favicon

--- softrave-clone/src/components/FrontEnd/Navbar.tsx
+++ softrave-clone/src/components/FrontEnd/Navbar.tsx
@@ -21,39 +21,49 @@ const Navbar = () => {
     { name: "contact", id: "footer" },
   ];
   return (
-    <nav className="w-full  bg-white hidden justify-between py-5 rounded-b-[40px] px-14 items-center sm:flex ">
-      <div
-        className="logo cursor-pointer "
-        onClick={() =>
-          pathname == "/" ? handleScroll("hero") : router.push("/")
-        }
-      >
-        <Image alt="nav-logo" width={160} height={30} src={"/svgs/logo.svg"} />
-      </div>
-      <ul className="flex gap-6 items-center ">
-        {links.map((link, index) => {
-          return (
-            <li
-              onClick={() => handleScroll(link.id)}
-              className={`px-6 py-2 bg-secondary text-[12px] font-semibold rounded-[10px] cursor-pointer ${
-                link.id == "footer" ? " hidden lg:block" : ""
-              }  `}
-              key={index}
-            >
-              {link.name}
-            </li>
-          );
-        })}
-      </ul>
-      <Link
-        href={
-          "https://api.whatsapp.com/send?phone=+923348177646&text=Hello,%20I%27m%20reaching%20out%20via%20Softrave.com%20to%20discuss%20a%20project."
-        }
-        className="contact-btn text-white bg-primary px-9 text-base tracking-wide duration-500 hover:-translate-y-1 hover:cursor-pointer   py-2.5 rounded-xl font-bold "
-      >
-        discuss the project
-      </Link>
-    </nav>
+    <>
+      <nav className="w-full  bg-white hidden justify-between py-5 rounded-b-[40px] px-14 items-center sm:flex ">
+        <div
+          className="logo cursor-pointer "
+          onClick={() =>
+            pathname == "/" ? handleScroll("hero") : router.push("/")
+          }
+        >
+          <Image
+            alt="nav-logo"
+            width={160}
+            height={30}
+            src={"/svgs/logo.svg"}
+          />
+        </div>
+        <ul className="flex gap-6 items-center ">
+          {links.map((link, index) => {
+            return (
+              <li
+                onClick={() => handleScroll(link.id)}
+                className={`px-6 py-2 bg-secondary text-[12px] font-semibold rounded-[10px] cursor-pointer ${
+                  link.id == "footer" ? " hidden lg:block" : ""
+                }  `}
+                key={index}
+              >
+                {link.name}
+              </li>
+            );
+          })}
+        </ul>
+        <Link
+          href={
+            "https://api.whatsapp.com/send?phone=+923348177646&text=Hello,%20I%27m%20reaching%20out%20via%20Softrave.com%20to%20discuss%20a%20project."
+          }
+          className="contact-btn text-white bg-primary px-9 text-base tracking-wide duration-500 hover:-translate-y-1 hover:cursor-pointer   py-2.5 rounded-xl font-bold "
+        >
+          discuss the project
+        </Link>
+      </nav>
+      <nav>
+        
+      </nav>
+    </>
   );
 };
 
